804ec2247c30aaf0c88de1e7d119fb00b4aa581a
JTSchwartz <jacob@jtschwartz.com>
2023-12-09 16:09:39 -0500

    Consolidation

--- src/main/kotlin/com/jtschwartz/aoc2023/days/Day09.kt
+++ src/main/kotlin/com/jtschwartz/aoc2023/days/Day09.kt
@@ -8,17 +8,12 @@ class Day09: Day(9) {
     override fun part1() = readInput(part = 1, isTest = false).p1()
     override fun part2() = readInput(part = 1, isTest = false).p2()
 
-    private fun List<String>.p1(): Long = parse().sumOf { it.reversed().predictTheFuture() }
-    private fun List<String>.p2(): Long = parse().sumOf { it.predictTheFuture() }
+    private fun List<String>.p1(): Long = parse().sumOf { it.reversed().predict() }
+    private fun List<String>.p2(): Long = parse().sumOf { it.predict() }
 
     private fun List<String>.parse(): List<List<Long>> = map { it.split("\\s+".toRegex()).toLongs() }
 
-    private fun List<Long>.predictTheFuture(): Long {
-       if (all { it == 0L }) {
-           return 0
-       }
-        return first() + steps().predictTheFuture()
-    }
+    private fun List<Long>.predict(): Long = if (all { it == 0L }) 0 else first() + steps().predict()
 
     private fun List<Long>.steps() = mapIndexedNotNull { index, current -> if (index == size - 1) null else current - get(index + 1) }
 }
